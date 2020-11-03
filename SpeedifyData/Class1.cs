bdfd070afd6cf6578b69f8ddca3ab1ec8ee6908b
Rani Kale <ranikl1991@gmail.com>
2020-11-03 21:44:43 +0530

    Add project files.

---
+++ SpeedifyData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SpeedifyData
+{
+    public class Class1
+    {
+    }
+}
---
+++ SpeedifyPersistance/Infrastructure/Repository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedifyPersistance.Infrastructure
+{
+    class Repository
+    {
+    }
+}
